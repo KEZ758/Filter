fb4f20389c7aaff1fd73515f1f18d79fe1b4d7c7
KEZ <yerkhan.kobenov@gmail.com>
2025-07-07 17:19:17 +0500

    z

--- src/main/java/kz/yerkhan/dev/Main.java
+++ src/main/java/kz/yerkhan/dev/Main.java
@@ -6,22 +6,17 @@ import java.util.List;
 public class Main {
     public static void main( String[] args ) {
         try {
-            // 1. Разбор аргументов
             ArgumentParser parser = new ArgumentParser(args);
 
-            // 2. Получение параметров
             List<String> inputFiles = parser.getInputFiles();
             String outputDir = parser.getOutputDirectory();
             String prefix = parser.getPrefix();
             boolean append = parser.isAppendMode();
 
-            // 3. Создание процессора
             FileProcessor processor = new FileProcessor(inputFiles, outputDir, prefix, append);
 
-            // 4. Обработка файлов
             processor.processFiles();
 
-            // 5. Вывод статистики
             if (parser.isFullStats()) {
                 processor.printFullStats();
             } else {
